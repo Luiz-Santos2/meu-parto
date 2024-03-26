b0c7fcf9c4d0ff883735bff1b0b09ebe518bac1f
Luiz-Santos2 <luizclovis2014@gmail.com>
2024-03-26 15:19:54 -0300

    ajuste de erro na integração

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import { NavegacaoPrincipal } from './src/navigations';
+//import { NavegacaoPrincipal } from './src/navigations';
 import { initializeApp } from '@firebase/app';
-import { firebaseConfig } from './src/config/firebase-config';
+//import { firebaseConfig } from './src/config/firebase-config';
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
-  initializeApp(firebaseConfig)
+  //initializeApp(firebaseConfig)
 
   return (
     <View style={styles.container}>
