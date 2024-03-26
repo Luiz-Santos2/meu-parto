25f18658eea1ac3b5aaacaa67f590a14758f18d5
Luiz-Santos2 <luizclovis2014@gmail.com>
2024-03-26 15:11:00 -0300

    Realizado integração com firebase

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
+import { NavegacaoPrincipal } from './src/navigations';
+import { initializeApp } from '@firebase/app';
+import { firebaseConfig } from './src/config/firebase-config';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  initializeApp(firebaseConfig)
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
---
+++ src/config/firebase-config.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAj9U8fIJFGUBw8Tcf5ZM6ulA2geZ-5RWc",
+    authDomain: "meu-parto.firebaseapp.com",
+    projectId: "meu-parto",
+    storageBucket: "meu-parto.appspot.com",
+    messagingSenderId: "799081790414",
+    appId: "1:799081790414:web:137872c85703e7b98fc99f",
+    measurementId: "G-J6CMH49QHX"
+  };
